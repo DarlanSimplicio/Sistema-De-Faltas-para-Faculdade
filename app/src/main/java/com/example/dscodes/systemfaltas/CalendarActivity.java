9bcd78c6e96726faa1b0f86510f5f0bf9b8df14e
VictorCelso <victorcelsoct@hotmail.com>
2018-04-11 00:12:31 -0300

    segundo commit

--- app/src/main/java/com/example/dscodes/systemfaltas/CalendarActivity.java
+++ app/src/main/java/com/example/dscodes/systemfaltas/CalendarActivity.java
@@ -1,5 +1,7 @@
 package com.example.dscodes.systemfaltas;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,20 +11,20 @@ import android.widget.TextView;
 import com.example.dscodes.systemfaltas.basics.Dados;
 
 public class CalendarActivity extends AppCompatActivity {
-
+    Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
+        CalendarView calendarView = findViewById(R.id.calendarView);
         calendarView.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-
+//                AlertDialog dialog = new AlertDialog(context);
             }
         });
-        TextView textView = (TextView)findViewById(R.id.textMateria);
+        TextView textView = findViewById(R.id.textMateria);
         textView.setText(((Dados)getApplication()).getSelectedMateria().getNome());
     }
 }
